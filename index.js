8a0902c6fb602f90b32b4ce5beec997f8305dc73
Louis Fourquet <louis@soixantecircuits.fr>
2017-01-19 14:33:29 +0100

    Added subscribe and unsubscribe commands

--- index.js
+++ index.js
@@ -4,23 +4,41 @@ const vorpal = require('vorpal')()
 const spacebroClient = require('spacebro-client')
 const config = require('./config')
 
-spacebroClient.connect( config.spacebro.address,
+spacebroClient.connect(config.spacebro.address,
                         config.spacebro.port,
-                        { clientName: 'spacebro-client-cli',
-                          channelName: config.channelName,
-                          verbose: false })
+  { clientName: 'spacebro-client-cli',
+    channelName: config.channelName,
+    verbose: false })
 
 vorpal
-  .command('emit <event> <data>', 'Emits a spacebro event.\n<event> is the event name\n<data> is JSON parsed data')
+  .command('subscribe <event>', 'Start listening to a specific spacebro event.')
+  .action(function (args, callback) {
+    spacebroClient.on(args.event, (data) => {
+      this.log('Received event "' + args.event + '" with data ' + data)
+    })
+    this.log('Subscribed to event "' + args.event + '"')
+    return callback()
+  })
+
+vorpal
+  .command('unsubscribe <event>', 'Stop listening to a specific spacebro event.')
+  .action(function (args, callback) {
+    spacebroClient.off(args.event)
+    this.log('Unsubscribed to event "' + args.event + '"')
+    return callback()
+  })
+
+vorpal
+  .command('emit <event> [data]', 'Emits a spacebro event with optionnal JSON parsed data.')
   .action(function (args, callback) {
     try {
-      var data = JSON.parse(args.data)
+      var data = (args.data) ? JSON.parse(args.data) : undefined
     } catch (e) {
-      this.log('Parsing Error in <data> argument:\n', e)
+      this.log("Parsing Error in 'data' argument:\n", e)
       return callback()
-    } 
+    }
     spacebroClient.emit(args.event, data)
-    this.log('Emitted event "', args.event, '" with data', data)
+    this.log('Emitted event "' + args.event + '" with data ' + data)
     return callback()
   })
 
